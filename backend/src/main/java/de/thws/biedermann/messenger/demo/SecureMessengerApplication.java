8b9ac1eb24d7944ed2b2f9976e80cbf108c79683
Jakob Priesner <jakob.priesner@outlook.de>
2023-04-04 18:42:17 +0200

    feat: create spring project

---
+++ backend/src/main/java/de/thws/biedermann/messenger/demo/SecureMessengerApplication.java
@@ -0,0 +1,13 @@
+package de.thws.biedermann.messenger.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureMessengerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/thws/biedermann/messenger/demo/SecureMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thws.biedermann.messenger.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
