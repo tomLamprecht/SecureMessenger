84404ca9711a26f0a690c40f79dcdc6d5c54ee84
Goll, Franziska <franziska.goll@student.fhws.de>
2023-05-05 16:04:46 +0200

    Feature friendship added tests (FG)

--- backend/src/main/java/de/thws/biedermann/messenger/demo/friendRequest/logic/FriendshipService.java
+++ backend/src/main/java/de/thws/biedermann/messenger/demo/friendRequest/logic/FriendshipService.java
@@ -1,10 +1,12 @@
 package de.thws.biedermann.messenger.demo.friendRequest.logic;
+
 import de.thws.biedermann.messenger.demo.friendRequest.model.Friendship;
 import de.thws.biedermann.messenger.demo.friendRequest.repository.FriendshipRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
