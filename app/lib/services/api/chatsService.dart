bbcf6888c755c542b65b2fabbd5da7152ab93184
TimNöth <tim.noeth@student.fhws.de>
2023-05-26 18:00:39 +0200

    rebase 1

--- app/lib/services/api/chatsService.dart
+++ app/lib/services/api/chatsService.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:developer';
 import 'package:http/http.dart' as http;
 
+import '../models/Chat.dart';
 import 'api_config.dart';
 
 class ChatsService {
@@ -16,6 +17,20 @@ class ChatsService {
       final chatId = json.decode(response.body)['chatId'];
       return chatId;
     } else {
+      return null;
+    }
+  }
+
+  Future<List<Chat>?> getChatsFromUser() async {
+    final url = Uri.parse('${ApiConfig.baseUrl}/chats');
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonList = json.decode(response.body);
+      final List<Chat> chatList = jsonList.map((json) => Chat.fromJson(json)).toList();
+      return chatList;
+    } else {
+      log("Keine Liste bei GET-Request erhalten!");
       return null;
     }
   }
