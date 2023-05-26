da7e5a9e4792091f9ae5655c66021c1599ae8bce
TimNöth <tim.noeth@student.fhws.de>
2023-05-26 19:13:15 +0200

    added ToDos - ChatOverview VM;FG;TN

--- app/lib/screens/chatOverview.dart
+++ app/lib/screens/chatOverview.dart
@@ -68,7 +68,8 @@ class _ChatOverviewPageState extends State<ChatOverviewPage> {
             ),
             onDismissed: (direction) {
               setState(() {
-                chats?.removeAt(index);
+                // ToDo: wollen wir hier chat löschen (also User aus Chat entfernen) oder chat archivieren machen? Oder des wegswipen als Funktion entfernen?
+                // chats?.removeAt(index);
               });
             },
             child: Hero(
@@ -86,7 +87,7 @@ class _ChatOverviewPageState extends State<ChatOverviewPage> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const ChatWidget(),
+                      builder: (context) => const ChatWidget(), // ToDo: wie weis die Klasse, welchen Chat wir brauchen?
                     ),
                   );
                 },
