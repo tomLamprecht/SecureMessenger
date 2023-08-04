e71b05608ddd90ebbb77f5ae4ea35e2c8a41de29
Jakob Priesner <jakob.priesner@outlook.de>
2023-08-04 10:21:34 +0200

    merge changes to master

--- backend/src/main/java/de/thws/securemessenger/repositories/ChatToAccountRepository.java
+++ backend/src/main/java/de/thws/securemessenger/repositories/ChatToAccountRepository.java
@@ -1,11 +1,14 @@
 package de.thws.securemessenger.repositories;
 
+import de.thws.securemessenger.model.Account;
 import de.thws.securemessenger.model.ChatToAccount;
+import io.micrometer.observation.ObservationFilter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatToAccountRepository extends JpaRepository<ChatToAccount, Long> {
+    ObservationFilter findChatToAccountByIdAndAccount(long chatId, Account account);
 
     // TODO refactor
     //Optional<ChatToAccount> findChatToUserByChatIdAndUserId(long userId, long chatId );
