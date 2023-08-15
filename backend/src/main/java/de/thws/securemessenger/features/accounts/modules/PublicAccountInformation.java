28c2e8029ac9b10caab4c7a2b1f309d78b7faddb
Jakob Priesner <jakob.priesner@outlook.de>
2023-08-15 23:41:53 +0200

    on working

--- backend/src/main/java/de/thws/securemessenger/features/accounts/modules/PublicAccountInformation.java
+++ backend/src/main/java/de/thws/securemessenger/features/accounts/modules/PublicAccountInformation.java
@@ -2,7 +2,7 @@ package de.thws.securemessenger.features.accounts.modules;
 
 import de.thws.securemessenger.model.Account;
 
-public record PublicAccountInformation(long accountId, String userName, String publicKey) {
+public record PublicAccountInformation(long id, String userName, String publicKey) {
 
     public PublicAccountInformation(Account account) {
         this(account.id(), account.username(), account.publicKey());
