6039d3aae14fb45487cf08884a4b64348cf2df00
tomLamprecht <79751041+tomLamprecht@users.noreply.github.com>
2023-09-16 15:37:27 +0200

    adding more logging

--- backend/src/main/java/de/thws/securemessenger/features/messenging/model/TimeSegment.java
+++ backend/src/main/java/de/thws/securemessenger/features/messenging/model/TimeSegment.java
@@ -5,21 +5,21 @@ import java.time.Instant;
 public record TimeSegment(Instant since, Instant till) {
 
     public TimeSegment {
-        if (since == null || till == null)
-            throw new IllegalArgumentException("null is not allowed!");
+        if ( since == null || till == null )
+            throw new IllegalArgumentException( "null is not allowed!" );
         if ( since.isAfter( till ) )
-            throw new IllegalArgumentException("since must be before or equal to till");
+            throw new IllegalArgumentException( "since must be before or equal to till (since=" + since + ") (till=" + till + ")" );
     }
 
     /**
      * @param instant
      * @return true if the instant is between since and till, or equal to one of them.
      */
-    public boolean contains(Instant instant) {
-        if (instant == null)
+    public boolean contains( Instant instant ) {
+        if ( instant == null )
             throw new IllegalArgumentException();
 
-        return !(since != null && instant.isBefore(since) || till != null && (instant.isAfter(till)));
+        return !( since != null && instant.isBefore( since ) || till != null && ( instant.isAfter( till ) ) );
     }
 
 }
