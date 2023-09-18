e184d5ceaa9b607682d6dd950bd7f06c19d0af13
tomLamprecht <79751041+tomLamprecht@users.noreply.github.com>
2023-09-18 18:36:33 +0200

    removed logging

--- app/lib/services/stores/account_information_store.dart
+++ app/lib/services/stores/account_information_store.dart
@@ -32,26 +32,19 @@ class AccountInformationStore {
   }
 
   Future<Uint8List?> getProfilePicByUsername(String accountName) async {
-    print("inside cache method");
     if (cachedPicAccounts.containsKey(accountName)) {
-      print("Found value for $accountName");
       return cachedPicAccounts[accountName];
     }
-    print("requesting pic for $accountName");
     var accountInformation = await AccountService().getAccountByUsername(accountName);
     if (accountInformation == null) {
       throw Exception("Got empty account.");
     }
     if (accountInformation.encodedProfilePic != null) {
-      print("Got a pic for $accountName caching it...");
       final imageDataBytes = Uint8List.fromList(base64Decode(accountInformation.encodedProfilePic!));
       cachedPicAccounts.putIfAbsent(accountName, () => imageDataBytes);
-      print("cached pics: ${cachedPicAccounts.length}");
       return imageDataBytes;
     } else {
-      print("had no pic for $accountName");
       cachedPicAccounts.putIfAbsent(accountName, () => null);
-      print("cached pics: ${cachedPicAccounts.length}");
       return null;
     }
   }
