f6cb93a4a95fbffe12f2453c734f6f181ef09f07
tomLamprecht <79751041+tomLamprecht@users.noreply.github.com>
2023-09-18 05:26:29 +0200

    added register validation

--- app/lib/screens/register_screen.dart
+++ app/lib/screens/register_screen.dart
@@ -9,7 +9,7 @@ import 'package:securemessenger/services/stores/ecc_key_store.dart';
 
 import '../services/files/cert_file_handler.dart';
 import 'chat_overview_screen.dart';
-
+import '../widgets/validated_text_field.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -23,19 +23,21 @@ class _RegisterScreenState extends State<RegisterScreen> {
   late TextEditingController _captchaController;
   late TextEditingController _certPasswordController;
   late String _captchaId;
+  ImageProvider<Object>? image;
   late CaptchaService _captchaService;
   late RegistrationService _registrationService;
   late String _publicKey;
   Keypair? _keyPair;
   final _certFileHandler = CertFileHandler();
-  final ValueNotifier<String?> _usernameValidationError = ValueNotifier<String?>(null);
-
-  final ValueNotifier<bool> _isDownloadButtonEnabled = ValueNotifier(false);
+  final ValueNotifier<String?> _usernameValidationError =
+  ValueNotifier<String?>(null);
+  bool _validCredentials = false;
 
   @override
   void initState() {
     super.initState();
     _userNameController = TextEditingController();
+    _userNameController.addListener(_onTextFieldChanged);
     _captchaController = TextEditingController();
     _certPasswordController = TextEditingController();
     _certPasswordController.addListener(_onTextFieldChanged);
@@ -53,7 +55,13 @@ class _RegisterScreenState extends State<RegisterScreen> {
   }
 
   void _onTextFieldChanged() {
-    _isDownloadButtonEnabled.value = _certPasswordController.text.isNotEmpty;
+    var tmp = _validCredentials;
+    _validCredentials = _validateUsername(_userNameController.text) == null &&
+        _validatePassword(_certPasswordController.text) == null;
+    if( _validCredentials != tmp)
+    {
+      setState(() {});
+    }
   }
 
   Future _generateKeys() async {
@@ -63,14 +71,42 @@ class _RegisterScreenState extends State<RegisterScreen> {
   }
 
   Future<ImageProvider> _loadCaptchaImage() async {
+    if (_captchaId != '') {
+      return image!;
+    }
     _captchaId = await _captchaService.getNewCaptcha();
-    var image = await _captchaService.fetchCaptchaImage(_captchaId);
-    return image;
+
+    image = await _captchaService.fetchCaptchaImage(_captchaId);
+    return image!;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password cannot be empty';
+    }
+
+    if (value.length < 8) {
+      return 'Password must be at least 8 characters long';
+    }
+
+    if (!RegExp(r'^(?=.*[a-z])(?=.*[A-Z])').hasMatch(value)) {
+      return 'Password must contain both upper and lower case letters';
+    }
+
+    if (!RegExp(r'^(?=.*\d)').hasMatch(value)) {
+      return 'Password must contain at least one numeric digit';
+    }
+
+    if (!RegExp(r'^(?=.*[@$!%*?&])').hasMatch(value)) {
+      return 'Password must contain at least one special character (@, \$, !, %, *, ?, &)';
+    }
+
+    return null;
   }
 
   String? _validateUsername(String? value) {
     if (value == null || value.isEmpty) {
-      return null;
+      return "Username can't be empty";
     }
     if (!RegExp(r'^[a-zA-Z0-9]+$').hasMatch(value)) {
       return 'Invalid username. Only letters and numbers are allowed!';
@@ -91,13 +127,14 @@ class _RegisterScreenState extends State<RegisterScreen> {
     }
 
     try {
-       await _registrationService.registerUser(
+      await _registrationService.registerUser(
           captchaId: _captchaId,
           captchaTry: _captchaController.text,
           publicKey: _publicKey,
           userName: userName);
 
-      await _certFileHandler.downloadCertificate(_keyPair!, "$userName.cert", _certPasswordController.text);
+      await _certFileHandler.downloadCertificate(
+          _keyPair!, "$userName.cert", _certPasswordController.text);
 
       EccKeyStore().publicKey = _keyPair!.publicKey;
       EccKeyStore().privateKey = _keyPair!.privateKey;
@@ -114,103 +151,90 @@ class _RegisterScreenState extends State<RegisterScreen> {
   void _redirectToChatOverview() {
     Navigator.pushAndRemoveUntil(
         context,
-        MaterialPageRoute(
-            builder: (context) => ChatOverviewPage()
-        ),
-        (route) => false
-    );
+        MaterialPageRoute(builder: (context) => ChatOverviewPage()),
+            (route) => false);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Register"), backgroundColor: Colors.blue),
+      appBar:
+      AppBar(title: const Text("Register"), backgroundColor: Colors.blue),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ValueListenableBuilder<String?>(
-              valueListenable: _usernameValidationError,
-              builder: (context, errorMessage, child) {
-                return TextField(
-                  controller: _userNameController,
-                  decoration: InputDecoration(
+                valueListenable: _usernameValidationError,
+                builder: (context, errorMessage, child) {
+                  return ValidatedTextField(
+                    controller: _userNameController,
+                    isPassword: false,
                     labelText: 'Username',
-                    errorText: _validateUsername(_userNameController.text),
-                  ),
-                  keyboardType: TextInputType.name,
-                  textCapitalization: TextCapitalization.words,
-                  maxLength: 50,
-                  maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                );
-              }
-              ),
-                TextField(
-                  controller: _certPasswordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Certificate Password',
-                  ),
-                  keyboardType: TextInputType.visiblePassword,
-                  textCapitalization: TextCapitalization.words,
-                  maxLength: 50,
-                  maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                ),
-                const SizedBox(height: 32),
-                FutureBuilder<ImageProvider>(
-                  future: _loadCaptchaImage(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    if (snapshot.hasError) {
-                      return const Text('Failed to load captcha image. Please try again later.');
-                    }
-                    return Stack(
-                      children: [
-                        SizedBox(
-                          height: 120,
-                          child: Image(
-                            image: snapshot.data!,
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          right: 0,
-                          child: FloatingActionButton(
-                            onPressed: () {
-                              setState(() {
-                                _captchaId = '';
-                              });
-                            },
-                            tooltip: 'Reload',
-                            child: const Icon(Icons.refresh),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-
-                const SizedBox(height: 16),
-                  TextField(
-                  controller: _captchaController,
-                  decoration: const InputDecoration(
-                    labelText: 'Captcha',
-                  ),
-                  maxLength: 7,
-                  maxLengthEnforcement: MaxLengthEnforcement.enforced
-                ),
-                const SizedBox(height: 32),
-                ElevatedButton(
-                  onPressed: _registerUser,
-                  child: const Text('Create user'),
-                ),
-              ],
+                    validationFunction: _validateUsername,
+                  );
+                }),
+            ValidatedTextField(
+              controller: _certPasswordController,
+              labelText: 'Password',
+              isPassword: true,
+              validationFunction: _validatePassword,
             ),
-          ),
+            const SizedBox(height: 32),
+            FutureBuilder<ImageProvider>(
+              future: _loadCaptchaImage(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.hasError) {
+                  return const Text(
+                      'Failed to load captcha image. Please try again later.');
+                }
+                return Stack(
+                  children: [
+                    SizedBox(
+                      height: 120,
+                      child: Image(
+                        image: snapshot.data!,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          setState(() {
+                            _captchaId = '';
+                          });
+                        },
+                        tooltip: 'Reload',
+                        child: const Icon(Icons.refresh),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+            const SizedBox(height: 16),
+            TextField(
+                controller: _captchaController,
+                decoration: const InputDecoration(
+                  labelText: 'Captcha',
+                ),
+                maxLength: 7,
+                maxLengthEnforcement: MaxLengthEnforcement.enforced),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: _validCredentials ? _registerUser : null,
+              child: const Text('Create user'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ app/lib/widgets/validated_text_field.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+typedef ValidationFunction = String? Function(String?);
+
+class ValidatedTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final ValidationFunction validationFunction;
+  final String labelText;
+  final bool isPassword;
+
+  const ValidatedTextField({
+    super.key,
+    required this.controller,
+    required this.validationFunction,
+    required this.labelText,
+    required this.isPassword,
+  });
+
+  @override
+  _ValidatedTextFieldState createState() => _ValidatedTextFieldState();
+}
+
+class _ValidatedTextFieldState extends State<ValidatedTextField> {
+  String? _errorMessage;
+
+  @override
+  void initState() {
+    _errorMessage = widget.validationFunction('');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        errorText: _errorMessage,
+      ),
+      keyboardType: widget.isPassword
+          ? TextInputType.visiblePassword
+          : TextInputType.name,
+      onChanged: (text) {
+        setState(() {
+          _errorMessage = widget.validationFunction(text);
+        });
+      },
+    );
+  }
+}
